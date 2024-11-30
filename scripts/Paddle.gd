529960585d281c979af2ac12c70f4d1ef8698df1
michelfournier <michel.fournier.trad@gmail.com>
2024-11-30 19:07:47 +0100

    modifcations done

--- scripts/Paddle.gd
+++ scripts/Paddle.gd
@@ -1,4 +1,4 @@
-extends CharacterBody2D
+extends Node2D
 
 @export var speed: float = 300
 
@@ -7,5 +7,4 @@ func _process(delta):
 		position.y -= speed * delta
 	if Input.is_action_pressed("ui_down"):
 		position.y += speed * delta
-	
 	
--- scripts/ball.gd
+++ scripts/ball.gd
@@ -1,11 +1,9 @@
-extends CollisionShape2D
+extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var velocity: Vector2 = Vector2(200, 150)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	position += velocity * delta
+	if position.y < 0 or position.y > 600: # Assuming 600 is screen height
+		velocity.y = -velocity.y
